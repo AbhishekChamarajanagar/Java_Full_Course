133863acc5ddeb7395d5ee1cc2101beb47564d68
Abhishek Chamarajanagar <abhishekchamarajanagar@gmail.com>
2026-08-08 00:40:03 +0530

    Swapping of two number

---
+++ swapTwoNumber.java
@@ -0,0 +1,11 @@
+public class swapTwoNumber {
+    public static void main(String[] args) {
+        int a =10;
+        int b=20;
+        System.out.println("Before swapping a=" + a + " and b=" + b);
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping a=" + a + " and b=" + b);
+    }
+}
