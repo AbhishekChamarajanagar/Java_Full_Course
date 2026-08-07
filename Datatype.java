264f7591aba82a6ac4c01e3b04eb95af0a3d224f
Abhishek Chamarajanagar <abhishekchamarajanagar@gmail.com>
2026-08-07 14:44:18 +0530

    created new file Datatype.java

---
+++ Datatype.java
@@ -0,0 +1,16 @@
+public class Datatype {
+    public static void main(String a[]){
+
+
+      int num1 = 3; //it is assignment operator it will take value right hand side it will assign the value to left hand side
+      int num2 = 3;
+      int result1 = num1 + num2;
+      int result2 = num1 - num2;
+      int result3 = num1 * num2;
+      int result4 = num1 / num2;
+      System.out.println(result1);
+      System.out.println(result2);
+      System.out.println(result3);
+      System.out.println(result4);
+    }
+}
