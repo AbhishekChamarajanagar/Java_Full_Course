cfd86bbeb5ba7de2b4a65346a289e75948afff9e
Abhishek Chamarajanagar <abhishekchamarajanagar@gmail.com>
2026-08-08 01:09:30 +0530

    for loop

---
+++ Loops.java
@@ -0,0 +1,16 @@
+public class Loops {
+    public static void main(String[] args) {
+        //System.out.println("Value of i: 1" );
+        //System.out.println("Value of i: 2" );
+        //System.out.println("Value of i: 3" );
+        //System.out.println("Value of i: 4" );
+        //System.out.println("Value of i: 5" );
+        //instead of printling 5 times we can use loop
+        //it terate over a value and print the value of i until the condition is true
+        //if condition is false then it will exit the loop
+        int i;
+        for( i=0;i<=5;i++){
+            System.out.println("Value of i: " + i);
+        }
+    }
+}
