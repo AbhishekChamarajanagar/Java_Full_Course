e3541331ef8cf706909f5fe15e38fb23b45f0bb1
Abhishek Chamarajanagar <abhishekchamarajanagar@gmail.com>
2026-08-07 20:26:33 +0530

    Even or Odd and also using ternary operator

---
+++ EvenorOdd.java
@@ -0,0 +1,19 @@
+public class EvenorOdd {
+    public static void main(String[] args) {
+        
+        // int num=10;
+        // if(num%2==0){
+        //     System.out.println("Even number");
+        // }
+        // else{
+        //     System.out.println("Odd number");
+        // }
+        
+
+        //we can use ternary operator to check even or odd number
+        int num=10;
+        String result = num % 2 == 0 ? "Even number" : "Odd number";
+        System.out.println(result);
+    }
+    
+}
