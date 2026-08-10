cd775d990fb93a16381cd68047fb87ca2005319f
Abhishek Chamarajanagar <abhishekchamarajanagar@gmail.com>
2026-08-10 18:36:34 +0530

    While loop

---
+++ 04_Loops/Whileloop.java
@@ -0,0 +1,32 @@
+// Outer loop increments → inner loop starts again from its initialized value → inner loop completes → outer loop increments again.
+
+// That's exactly how this  nested while loop works.
+// Outer loop: 1 iteration
+//     ↓
+//     Inner loop: FULL cycle
+//     ↓
+// Outer loop: next iteration
+//     ↓
+//     Inner loop: FULL cycle
+//     ↓
+// Outer loop: next iteration
+//     ↓
+//     Inner loop: FULL cycle
+
+public class Whileloop {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 5) {  // outer loop
+            System.out.println("hi: " + i);
+            int j =1;
+            while (j <= 3) { // inner loop
+                System.out.println("Hello: " + j);
+                j++;
+            }
+            i++;
+        }
+        System.out.println(" loop exit because condition becomes false and the Value of i becomes : " + i); // here i becomes 6 and the loop will exit because the condition is false
+    }
+}
